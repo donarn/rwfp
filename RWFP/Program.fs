b8bcba7d038c220c34677b87abdee0fc48e27454
Don Arnold <darnold992000@yahoo.com>
2016-05-20 17:10:43 -0500

    completed prog

--- RWFP/Program.fs
+++ RWFP/Program.fs
@@ -7,6 +7,7 @@
 //    0 // return an integer exit code
 
 open System
+open System.IO
 
 let convertDataRow(csvLine:string) =
     let cells = List.ofSeq(csvLine.Split(','))
@@ -35,16 +36,6 @@ let rec calculateSum(rows) =
         let remainingSum = calculateSum(tail)
         value + remainingSum
 
-//open System.IO
-//
-//let lines = List.ofSeq(File.ReadAllLines(@"c:\temp4\rwfp_ch03_data.csv"))
-//let data = processLines(lines)
-//let sum = float(calculateSum(data))
-//
-//for (title, value) in data do
-//    let percentage = int((float(value)) / sum * 100.0)
-//    Console.WriteLine("{0,-18} - {1,8} ({2}%)", title, value, percentage)
-
 open System
 open System.Drawing
 open System.Windows.Forms
@@ -68,6 +59,7 @@ let rnd = new Random()
 
 let randomBrush() =
     let r, g, b = rnd.Next(256), rnd.Next(256), rnd.Next(256)
+    printfn "%d %d %d" r g b
     new SolidBrush(Color.FromArgb(r,g,b))
 
 let drawPieSegment(gr:Graphics, title, startAngle, occupiedAngle) =
@@ -75,11 +67,65 @@ let drawPieSegment(gr:Graphics, title, startAngle, occupiedAngle) =
     gr.FillPie(br, 170, 70, 260, 260, startAngle, occupiedAngle)
     br.Dispose()
 
+let drawStep(drawingFunc, gr:Graphics, sum, data) =
+    let rec drawStepUtil(data, angleSoFar) = 
+        match data with
+        | [] -> ()
+        | [title, value] -> // special case for last item to compensate for rounding errors
+            let angle = 360 - angleSoFar
+            drawingFunc(gr, title, angleSoFar, angle)
+        | (title, value):: tail ->
+            let angle = int(float(value) / sum * 360.0)
+            drawingFunc(gr, title, angleSoFar, angle)
+            drawStepUtil(tail, angleSoFar + angle)
+    drawStepUtil(data, 0)
 
 
+let drawLabel(gr:Graphics, title, startAngle, angle) =
+    let fnt = new Font("Times New Roman", 11.0f)
+    let centerX, centerY = 300.0, 200.0
+    let labelDistance = 150.0
+
+    let lblAngle = float(startAngle + angle/2)
+    let ra = Math.PI * 2.0 * lblAngle / 360.0
+    let x = centerX + labelDistance * cos(ra)
+    let y = centerY + labelDistance * sin(ra)
+    let size = gr.MeasureString(title, fnt)
+    let rc = new PointF(float32(x) - size.Width / 2.0f, float32(y) - size.Height / 2.0f)
+
+    gr.DrawString(title, fnt, Brushes.Black, new RectangleF(rc, size))
+
+let drawChart(file) =
+    let lines = List.ofSeq(File.ReadAllLines(file))
+    let data = processLines(lines)
+    let sum = float(calculateSum(data))
+
+    let pieChart = new Bitmap(600, 400)
+    let gr = Graphics.FromImage(pieChart)
+
+    gr.Clear(Color.White)
+    drawStep(drawPieSegment, gr, sum, data)
+    drawStep(drawLabel, gr, sum, data)
+    gr.Dispose()    // finalizes drawing
+
+    pieChart
+
+let openAndDrawChart(e) =
+    let dlg = new OpenFileDialog(Filter="CSV Files|*.csv")
+    if (dlg.ShowDialog() = DialogResult.OK) then
+        let pieChart = drawChart(dlg.FileName)
+        boxChart.Image <- pieChart
+        btnSave.Enabled <- true
+
+let saveDrawing(e) =
+    let dlg = new SaveFileDialog(Filter="PNG Files|*.png")
+    if (dlg.ShowDialog() = DialogResult.OK) then
+        boxChart.Image.Save(dlg.FileName)
 
 [<STAThread>]
 do
+    btnOpen.Click.Add(openAndDrawChart) // event handler
+    btnSave.Click.Add(saveDrawing)      // ditto
     Application.Run(mainForm)
 
-
+// file : @"c:\temp4\rwfp_ch03_data.csv"
