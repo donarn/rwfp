36b070662903739405df8128c48b0db287bfe6d5
Don Arnold <darnold992000@yahoo.com>
2016-05-22 12:43:16 -0500

    just a commit

--- RWFP/Program.fs
+++ RWFP/Program.fs
@@ -1,10 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-//[<EntryPoint>]
-//let main argv = 
-//    printfn "%A" argv
-//    0 // return an integer exit code
+
 
 open System
 open System.IO
@@ -123,6 +120,7 @@ let saveDrawing(e) =
         boxChart.Image.Save(dlg.FileName)
 
 [<STAThread>]
+[<EntryPoint>]
 do
     btnOpen.Click.Add(openAndDrawChart) // event handler
     btnSave.Click.Add(saveDrawing)      // ditto
